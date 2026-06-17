75c7702066eef46599163433c9dc0c6454fbfdb5
MaikelBoezer220755 <220755@buas.nl>
2026-06-17 16:54:36 +0200

    update

---
+++ setup.bat
@@ -0,0 +1,8 @@
+mkdir C:\esports\sftp_data\emotion
+mkdir C:\esports\sftp_data\video
+mkdir C:\esports\sftp_data\input
+mkdir C:\esports\sftp_data\merged
+mkdir C:\esports\sftp_data\gaze
+mkdir C:\esports\sftp_data\eda
+mkdir C:\esports\sftp_data\audio
+docker compose up -d
